cf8c631be2b9361bfae02180e43e2bce684cbeea
Sakita1214 <y5eh5tmc4@gmail.com>
2022-05-17 13:19:47 +0000

    絞り込み検索調整

--- app/controllers/public/sights_controller.rb
+++ app/controllers/public/sights_controller.rb
@@ -5,6 +5,30 @@ class Public::SightsController < ApplicationController
     @post = Post.new
     @posts = Post.all
     @categories = Category.all
+
+    if params[:search].present?
+      post = Post.posts_serach(params[:search])
+    elsif params[:category_id].present?
+      @post = Post.find(params[:category_id])
+      post = @post.posts.order(created_at: :desc)
+    else
+      post = Post.all.order(created_at: :desc)
+    end
+    if params[:categories].present?
+      post_ids = []
+      params[:categories].each_with_index do |category_id,i|
+        if i == 0
+          post_ids =  Post.joins(:post_categories).where(post_categories:{category_id: category_id}).pluck(:id)
+          pp post_ids
+        else
+          post_ids =  Post.joins(:post_categories).where(post_categories:{category_id: category_id}).where(id: post_ids).pluck(:id)
+          pp post_ids
+        end
+      end
+      @posts = Post.find(post_ids)
+    end
+    #@post = Kaminari.paginate_array(items).page(params[:page]).per(10)
+
   end
 
 
