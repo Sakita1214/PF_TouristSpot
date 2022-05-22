9fc009b8caf3b14abc0a256ca5e7020eacc7b7ef
Sakita1214 <y5eh5tmc4@gmail.com>
2022-05-22 13:41:45 +0000

    絞り込み検索調整

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -18,9 +18,26 @@ class Public::PostCommentsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @post_comment = PostComment.find(params[:id])
+  end
+
+
   def destroy
     PostComment.find(params[:id]).destroy
     redirect_to post_path(params[:post_id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @post_comment = PostComment.find(params[:id])
+      if @post_comment.update(post_comment_params)
+        redirect_to edit_post_post_comment_path(@post.id, @post_comment.id), notice: "コメントを編集しました"
+      else
+        flash.now[:danger] = "編集に失敗しました"
+        render 'edit'
+      end
   end
 
   private
